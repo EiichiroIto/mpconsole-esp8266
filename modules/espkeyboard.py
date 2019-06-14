ab42f405d9c8efba0a154d25030999f65f008659
Eiichiro Ito <eiichiro.ito@gmail.com>
2019-06-14 16:30:16 +0900

    add startup scripts.

---
+++ modules/espkeyboard.py
@@ -0,0 +1,12 @@
+import ili9341
+from machine import Pin, SPI
+spi = SPI(1, baudrate=40000000, polarity=0, phase=0)
+display = ili9341.ILI9341(spi, cs=Pin(15), dc=Pin(2), rst=Pin(0), width=320, height=240)
+import ili9341_fb
+fb = ili9341_fb.Ili9341Framebuffer(display)
+import ps2
+ps2.init(clock=5, data=4)
+from fbconsole import FBConsole
+console = FBConsole(fb)
+import os
+os.dupterm(console)
