6e7541bfcb6ad41f07e8da471ac3cdb763839c92
Eiichiro Ito <eiichiro.ito@gmail.com>
2019-06-21 13:12:57 +0900

    Change gpio assignments.

--- modules/mpconsole.py
+++ modules/mpconsole.py
@@ -1,11 +1,11 @@
 import ili9341
 from machine import Pin, SPI
 spi = SPI(1, baudrate=40000000, polarity=0, phase=0)
-display = ili9341.ILI9341(spi, cs=Pin(15), dc=Pin(2), rst=Pin(0), width=320, height=240)
+display = ili9341.ILI9341(spi, cs=Pin(15), dc=Pin(16), rst=None, width=320, height=240)
 import ili9341_fb
 fb = ili9341_fb.Ili9341Framebuffer(display)
 import ps2
-ps2.init(clock=5, data=4)
+ps2.init(clock=5, data=0)
 from fbconsole import FBConsole
 console = FBConsole(fb)
 import os
